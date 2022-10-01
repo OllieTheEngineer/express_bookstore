8dc360fbaae5617ceb9841eecfe7e749518701b5
ollie <olivinanzali07@gmail.com>
2022-10-01 11:39:39 -0400

    adding bookstore files

---
+++ routes/books.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const Book = require("../models/book");
+
+const router = new express.Router();
+
+
+/** GET / => {books: [book, ...]}  */
+// getting all books
+
+router.get("/", async function (req, res, next) {
+  try {
+    const books = await Book.findAll(req.query);
+    return res.json({ books });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[id]  => {book: book} */
+// getting a book using an id
+
+router.get("/:isbn", async function (req, res, next) {
+  try {
+    const book = await Book.findOne(req.params.id);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST /   bookData => {book: newBook}  */
+// creating a book
+
+router.post("/", async function (req, res, next) {
+  try {
+    const book = await Book.create(req.body);
+    return res.status(201).json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PUT /[isbn]   bookData => {book: updatedBook}  */
+// updating a book
+
+router.put("/:isbn", async function (req, res, next) {
+  try {
+    const book = await Book.update(req.params.isbn, req.body);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[isbn]   => {message: "Book deleted"} */
+// deletinga book from database
+
+router.delete("/:isbn", async function (req, res, next) {
+  try {
+    await Book.remove(req.params.isbn);
+    return res.json({ message: "Book deleted" });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
